ed1f5539e06829696cd1fa1e7c16fc8eefa19643
Ionut <ionutzncr@gmail.com>
2022-01-26 13:24:27 +0100

    Create row using recursion function

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const container = document.querySelector(".container");
+const secondContainer = document.createElement("div")
 
-function createDiv(){
-    const div = document.createElement("div");
-    container.appendChild(div);
-}
 
 function createRow(numberOfDiv){
+    
+
+    function createDiv(){
+        
+        const div = document.createElement("div");
+        secondContainer.appendChild(div);
+    }
     
     console.log(numberOfDiv)
     if(numberOfDiv == 1) {
         
         createDiv()
+        container.appendChild(secondContainer)
+        
     } 
     else {
+       
         createDiv()
         createRow(numberOfDiv-1)
     } 
+    
 }
 createRow(16)
