cfc28a064527fdcad7b60f98716a91d016b444c1
Ionut <ionutzncr@gmail.com>
2022-01-27 10:53:54 +0100

    Add reset button

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const container = document.querySelector(".container");
 
 
 //initialisation of the function parameter 
-let nmbrOfDiv  = 16;
-let nmbrOfElementsInArow = nmbrOfDiv;
+let nbrOfDiv  = 16;
+let nbrOfElementsInArow= nbrOfDiv;
 let nbrOfColumn = 16;
 
 function createColumn(numberOfColumn){
-    const secondContainer = document.createElement("div")
-    secondContainer.setAttribute("class","secondContainer")
+    const secondContainer = document.createElement("div");
+    secondContainer.setAttribute("class","secondContainer");
     // It will repeat createSingleDiv() 
     function createRow(numberOfRepetition){
     
     function createSingleDiv(){
         const div = document.createElement("div");
         
-        secondContainer.appendChild(div)
+        secondContainer.appendChild(div);
         
     }
     //recursion Time
@@ -27,16 +27,16 @@ function createColumn(numberOfColumn){
     } else {
         createSingleDiv();
         
-        createRow(numberOfRepetition-1)
+        createRow(numberOfRepetition-1);
         }
     }
 
 
     //recursion for Column 
     if(numberOfColumn==1){
-        createRow(nmbrOfDiv);
+        createRow(nbrOfDiv);
     } else {
-        createRow(nmbrOfDiv);
+        createRow(nbrOfDiv);
         createColumn(numberOfColumn-1);
     }
 }
@@ -44,9 +44,54 @@ function createColumn(numberOfColumn){
 
 createColumn(nbrOfColumn);
 
-const smallDiv = document.querySelectorAll(".secondContainer div")
-console.log(smallDiv)
+// add size because I couldn't do it inside the recursion 
+// add event listener to add effect and reset 
+const smallDiv = document.querySelectorAll(".secondContainer div");
+console.log(smallDiv);
 smallDiv.forEach(div=>{
-    div.style.width = `${(600/nmbrOfElementsInArow)}px`;
-    div.style.height = `${(600/nmbrOfElementsInArow)}px`;
+    div.style.width = `${(600/nbrOfElementsInArow)}px`;
+    div.style.height = `${(600/nbrOfElementsInArow)}px`;
+    div.addEventListener("mouseenter",mouseIn);
+    div.addEventListener("mouseout",mouseOut);
 })
+
+function mouseIn(e){
+    e.currentTarget.style.backgroundColor  = "red";
+}
+
+function mouseOut(e){
+    e.currentTarget.style.backgroundColor = "white";
+}
+
+//add reset boutton that will reset the grid size 
+
+const button  = document.querySelector("button")
+const scdContainer = document.querySelectorAll(".secondContainer")
+
+button.addEventListener("click",resetScreen)
+
+function resetScreen(){
+    const scdContainer = document.querySelectorAll('.secondContainer')
+    scdContainer.forEach(div=>container.removeChild(div))
+    //reset functions parameter to new values then recreate the grid with createColumn
+    nbrOfColumn = +prompt("Height"); // a number
+    nbrOfDiv = +prompt("Width"); // a number
+    nbrOfElementsInArow= nbrOfColumn; // a number
+   
+    
+    if (nbrOfColumn == nbrOfDiv && nbrOfColumn<101){
+        createColumn(nbrOfColumn);
+        const container_nb_2 = document.querySelectorAll(".secondContainer>div")
+        //small div is not empty but because you removed every secondContainer from the dom there are'nt any other element of the array in the DOM.
+        console.log(container_nb_2)
+        container_nb_2.forEach(div=>{
+            div.style.width = `${(600/nbrOfElementsInArow)}px`;
+            div.style.height = `${(600/nbrOfElementsInArow)}px`;
+            div.addEventListener("mouseenter",mouseIn);
+            div.addEventListener("mouseout",mouseOut);
+        })
+    } else {
+        resetScreen()
+    }
+
+}
